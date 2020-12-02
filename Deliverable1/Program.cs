3d391c07bf9338529ca1d54e41d29dcfaaa1e1a2
Darrius horton <darriushorton@Darriuss-Air.attlocal.net>
2020-12-02 15:09:04 -0500

    updated

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -9,7 +9,7 @@ namespace Deliverable1
             Console.WriteLine("Please create a password containing a mmin of 7 chracters, maximun of 12 chracters, one upper and lower case letter and one special character");
 
 
-
+           
             string password;
 
             password = Console.ReadLine();
@@ -32,32 +32,36 @@ namespace Deliverable1
                 Console.WriteLine("error");
             }
 
-            //Need to fix my for each iteration
+            //Need to fix for each iteration
 
             foreach(char c in password)
             {
-                bool isUp = false;
-                bool isLow = false;
+               bool isUp = false;
+               bool isLow = false;
 
-               if(password(c).IsUpper())
+              if(char.IsUpper(c))
                 {
                     isUp = true;
+                    
                 }
 
-               if(password(c).IsUpper())
+              if(char.IsLower(c))
                 {
                     isLow = true;
+                    
                 }
 
+                if (password.Contains("!") && password.Length >= 7 && password.Length <= 12 && isUp == true && isLow == true)
+                {
+                    Console.WriteLine("Password Valid and accepted");
+                }
+
+
+
             }
 
 
 
-
-            if (password.Contains("!") && password.Length >= 7 && password.Length <= 12 && isUp == true && isLow == true)
-            {
-                Console.WriteLine("Password Valid and accepted");
-            }
 
 
 
